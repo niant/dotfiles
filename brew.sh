81ca52a551a91af41471c2e9742036ac79fe1199
niant <antti.niemenpaa@gmail.com>
2013-05-06 09:29:38 +0300

    Add PHP & MySQL installation

--- brew.sh
+++ brew.sh
@@ -10,5 +10,14 @@ brew install git
 brew install mongodb
 brew install wget
 
+# PHP environment
+# brew tap homebrew/dupes
+# brew tap josegonzalez/homebrew-php
+# brew install php54
+# brew install mysql
+## Install MySQL
+# unset TMPDIR
+# mysql_install_db --verbose --user=whoami --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
+
 # Remove outdated versions from the cellar
 brew cleanup
