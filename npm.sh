e2b8bbe58b442bb4e107bc5013f5172fbf97bbb2
niant <antti.niemenpaa@gmail.com>
2013-09-04 19:48:06 +0300

    Update to Node v0.10.17

--- npm.sh
+++ npm.sh
@@ -8,4 +8,4 @@ npm install -g grunt-cli
 npm install -g yo
 npm install -g generator-webapp
 npm install -g generator-angular
-npm install -g generator-karma@0.3 #  Peer generator-angular@0.3.0 wants generator-karma@~0.3.0
+npm install -g generator-karma #@0.3 #  Peer generator-angular@0.3.0 wants generator-karma@~0.3.0
