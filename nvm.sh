ed4aa618033081891951603ae7397c72cc419f19
niant <antti.niemenpaa@gmail.com>
2014-01-10 10:38:23 +0200

    Update Node.js version.

--- nvm.sh
+++ nvm.sh
@@ -3,6 +3,6 @@
 # Install nvm with curl:
 # curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-nvm install 0.10.17
-nvm use 0.10.17
-nvm alias default 0.10.17
+nvm install 0.10.24
+nvm use 0.10.24
+nvm alias default 0.10
