f9623792efab4c33561a789d979ea79db3b62e17
niant <antti.niemenpaa@gmail.com>
2014-02-18 22:18:18 +0200

    Add graspjs and pageres to installable npm packages.

--- npm.sh
+++ npm.sh
@@ -9,9 +9,11 @@ npm install -g mocha # testing framework
 npm install -g yo
 npm install -g generator-webapp
 npm install -g generator-angular
-npm install -g generator-karma #@0.3 #  Peer generator-angular@0.3.0 wants generator-karma@~0.3.0
+npm install -g generator-karma
 
 npm install -g http-server
 npm install -g npm-check-updates
 npm install -g node-inspector
 npm install -g nodemon
+npm install -g grasp
+npm install -g pageres
