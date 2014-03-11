b4a6e4c4c1cf4cbc4e68dbf314550b1d591e0b50
niant <antti.niemenpaa@gmail.com>
2014-03-11 16:07:22 +0200

    Remove unnecessary global NPM packages.

--- npm.sh
+++ npm.sh
@@ -2,16 +2,14 @@
 
 # Install global node.js packages for development
 npm install -g bower
-npm install -g karma
 npm install -g grunt-cli
-npm install -g mocha # testing framework
-
+npm install -g gulp
 npm install -g yo
-npm install -g generator-webapp
-npm install -g generator-angular
-npm install -g generator-karma #@0.3 #  Peer generator-angular@0.3.0 wants generator-karma@~0.3.0
+npm install -g karma-cli
 
+# node development
+# npm install -g node-inspector
+# npm install -g nodemon
+
+#Utils
 npm install -g http-server
-npm install -g npm-check-updates
-npm install -g node-inspector
-npm install -g nodemon
