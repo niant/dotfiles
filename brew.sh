2990249266c8539cf8757ad9525d0a5b8abaa999
niant <antti.niemenpaa@gmail.com>
2014-06-06 23:04:07 +0300

    Add Vim as installable package.

--- brew.sh
+++ brew.sh
@@ -15,6 +15,7 @@ brew install youtube-dl
 brew install tmux
 brew install reattach-to-user-namespace # http://savanne.be/804-running-karma-and-chrome-under-tmux-on-osx/
 brew install zsh
+brew install vim
 
 brew install phantomjs
 brew install casperjs --devel # casperjs installation
