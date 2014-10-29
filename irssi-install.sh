591f81586ee6bdc3183d4c99daba5aea9c4fcc0b
niant <antti.niemenpaa@gmail.com>
2014-10-29 22:29:30 +0200

    Add irssi installation scripts.

---
+++ irssi-install.sh
@@ -0,0 +1,3 @@
+curl -O "http://scripts.irssi.org/scripts/nicklist.pl" ~/.irssi/scripts/autorun/
+curl -O "http://dave.waxman.org/irssi/xchatnickcolor.pl" ~/.irssi/scripts/autorun/
+curl -O "http://scripts.irssi.org/scripts/hilightwin.pl" ~/.irssi/scripts/autorun/
