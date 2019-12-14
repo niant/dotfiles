4de7c30d4c297d96daed0b51fa69deb249603ec7
niant <antti.niemenpaa@gmail.com>
2019-12-14 16:20:07 +0200

    Remove unsupported code

--- macos.sh
+++ macos.sh
@@ -18,14 +18,6 @@
 
 # General UI/UX
 
-# Menu bar: hide the Time Machine, Volume and User icons
-for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-    defaults write "${domain}" dontAutoLoad -array \
-        "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-        "/System/Library/CoreServices/Menu Extras/Volume.menu" \
-        "/System/Library/CoreServices/Menu Extras/User.menu"
-done
-
 # defaults write com.apple.systemuiserver menuExtras -array \
 #     "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
 #     "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
